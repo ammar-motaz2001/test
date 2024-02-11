d31df24057276165ce350dccec4f3d34e97b391a
AmmarMotaz <127546796+ammar-motaz2001@users.noreply.github.com>
2024-02-11 14:49:51 +0200

    Layout.jsx

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -2,26 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from '../Footer/Footer'
-// function Root() {
-//   return (
-//     <div className="App">
-//       <nav>
-//         <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
-//         <NavLink to="/CONTACT" className={({ isActive }) => (isActive ? 'active' : '')}>Blog</NavLink>
-//         <NavLink to="/ABOUT" className={({ isActive }) => (isActive ? 'active' : '')}>About</NavLink>
-//         <NavLink to="/PORTFOLIO" className={({ isActive }) => (isActive ? 'active' : '')}>Contact</NavLink>
-//       </nav>
-//       <main>
-//         <Outlet />
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default Root;
-
-
-
 export default function Layout() {
   return (
     <div>
